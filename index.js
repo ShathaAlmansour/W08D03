5f6663a17096121c5af54b842953023a4eda283f
ShathaAlmansour <>
2021-11-30 15:08:09 +0300

    login and signup

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-require("./db")
-
+require("./db");
 
 const app = express();
 app.use(express.json());
@@ -12,11 +11,11 @@ app.use(cors());
 
 const roleRouter = require("./routers/routes/role");
 app.use(roleRouter);
+const userRouter = require("./routers/routes/user");
+app.use(userRouter);
 
 const PORT = process.env.PORT || 4000;
-app.use(cors());
 
 app.listen(PORT, () => {
-    console.log(`server running at port ${PORT}`);
-  });
-  
+  console.log(`server running at port ${PORT}`);
+});
---
+++ routers/controllers/user.js
@@ -0,0 +1,62 @@
+const userModel = require("./../../db/models/user");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const signup = async (req, res) => {
+  const { email, password, role } = req.body;
+
+  const savedEmail = email.toLowerCase();
+  const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT));
+
+  const newUser = new userModel({
+    email: savedEmail,
+    password: hashedPassword,
+    role,
+  });
+
+  newUser
+    .save()
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+};
+const signin = (req, res) => {
+  const { email, password } = req.body;
+
+  const savedEmail = email.toLowerCase();
+
+  userModel
+    .findOne({ email: savedEmail })
+    .then(async (result) => {
+      if (result) {
+        if (result.email == savedEmail) {
+          const checkedPassword = await bcrypt.compare(
+            password,
+            result.password
+          );
+          if (checkedPassword) {
+            const payload = { role: result.role };
+            const options = { expiresIn: "1h" };
+            const secret = process.env.SECRET;
+            const token = await jwt.sign(payload, secret, options);
+            res.status(200).send({ result, token });
+          } else {
+            res.status(404).send("Invalid email or password");
+          }
+        } else {
+          res.status(404).send("Invalid email or password");
+        }
+      } else {
+        res.status(404).send("User doesn't exist");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+};
+
+module.exports = { signup, signin };
