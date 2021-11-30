0e24002771408b17857f04a2a11614f117bb2803
ShathaAlmansour <>
2021-11-30 14:20:20 +0300

    taskSchema

--- db/models/user.js
+++ db/models/user.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
+  task: { type: mongoose.Schema.Types.ObjectId, ref: "Task" },
+
 });
 
 module.exports = mongoose.model("User", userSchema);
