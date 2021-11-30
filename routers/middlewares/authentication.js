175aa4941bef8b2a10da99a2e4ff9558ffe9ecb8
ShathaAlmansour <>
2021-11-30 16:11:33 +0300

    authentication

---
+++ routers/middlewares/authentication.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const secret = process.env.secretKey;
+const authentication = (req, res, next) => {
+  try {
+    if (!req.headers.authorization)
+      return res.status(403).send({ message: "forbidden" });
+    const token = req.headers.authorization.split(" ")[1];
+    const parsedToken = jwt.verify(token, secret);
+    req.token = parsedToken;
+    next();
+  } catch (error) {
+    res.status(403).send(error);
+  }
+}
+module.exports = authentication;
