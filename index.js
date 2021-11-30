df2eea400119294f6ede34c28714dcdfe46bb3ba
ShathaAlmansour <>
2021-11-30 19:54:49 +0300

    task

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ app.use(cors());
 
 const roleRouter = require("./routers/routes/role");
 app.use(roleRouter);
+
 const userRouter = require("./routers/routes/user");
 app.use(userRouter);
+
+const taskRouter = require("./routers/routes/task");
+app.use(taskRouter);
 
 const PORT = process.env.PORT || 4000;
 
